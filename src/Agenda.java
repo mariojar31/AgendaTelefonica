de0af6a298ff652ccc941527b751d0aab9993ab7
mariojar31 <mariojoacre31@gmail.com>
2024-10-10 17:02:38 -0500

    Update Final

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Agenda {
     int cantContactos;
@@ -48,20 +50,26 @@ public class Agenda {
 
 
         }else if(espaciosLibres()==0){
-            System.out.println("\n----------------------------------------\n");
+            System.out.println("\n---------------------------------------------------------------\n");
             System.out.println("Agenda llena. Elimine contactos para seguir agregando nuevos.");
-            System.out.println("\n----------------------------------------\n");
+            System.out.println("\n---------------------------------------------------------------\n");
         }else if(estaEnAgenda(contacto)){
-            System.out.println("\n----------------------------------------\n");
-            System.out.println("El contacto ya se encuentra registrado en la agenda.");
-            System.out.println("\n----------------------------------------\n");
+            System.out.println("\n---------------------------------------------------------------\n");
+            System.out.println("¡Contacto No agregado!");
+            System.out.println("El contacto ya se encuentra previamente registrado en la agenda.");
+            System.out.println("\n--------------------------------------------------------------\n");
         }
     }
 
     public void listarContactos(){
         System.out.println("\n::::::::::::::::Contactos::::::::::::::::\n");
-        
-        for (Contacto contacto : contactos){
+         // Filtrar contactos no nulos
+        Contacto[] contactosFiltrados = Arrays.stream(contactos)
+        .filter(contacto -> contacto != null)
+        .toArray(Contacto[]::new);
+
+        Arrays.sort(contactosFiltrados, Comparator.comparing(Contacto::getNombre));
+        for (Contacto contacto : contactosFiltrados){
             if(contacto!=null){
                 System.out.println(contacto.getNombre()+" "+contacto.getApellido());
                 System.out.println("Telefono: "+contacto.getTelefono());
@@ -71,6 +79,103 @@ public class Agenda {
         }
         System.out.println("Contactos guardados: "+(cantContactos-espaciosLibres()));
         System.out.println("Espacio disponible: "+espaciosLibres()+" de "+cantContactos+"\n");
+    }
+
+    public Contacto buscarContacto(String nombre, String apellido){
+        boolean esta=false;
+        Contacto contactoBuscado = null;
+
+        for (Contacto contacto : contactos) {
+            if(contacto!=null && contacto.getNombre().toLowerCase().equals(nombre.toLowerCase()) && contacto.getApellido().toLowerCase().equals(apellido.toLowerCase())){
+                System.out.println("\n----------------------------------------\n");
+                System.out.println("Contacto encontrado:\n");
+                System.out.println(contacto.getNombre()+" "+contacto.getApellido());
+                System.out.println("Telefono: "+contacto.getTelefono());
+                System.out.println("\n----------------------------------------\n");
+                contactoBuscado=contacto;
+                esta=true;
+                break;
+            }
+        }
+        if (!esta) {
+            System.out.println("\n----------------------------------------\n");
+            System.out.println("...Contacto no encontrado...");
+            System.out.println("\n----------------------------------------\n");
+            
+        }
+        
+        return contactoBuscado;
+        
+    }
+
+    public Contacto obtenerContacto(String nombre, String apellido){
+
+        Contacto contactoBuscado = null;
+
+        for (Contacto contacto : contactos) {
+            if(contacto!=null && contacto.getNombre().toLowerCase().equals(nombre.toLowerCase()) && contacto.getApellido().toLowerCase().equals(apellido.toLowerCase())){
+                contactoBuscado=contacto;
+                break;
+            }
+        }
+        
+        return contactoBuscado;
+        
+    }
+
+    public void eliminarContacto(Contacto contactoe) {
+        if(contactoe!=null){
+            for (int i = 0; i < contactos.length; i++) {
+                if (contactos[i] != null
+                        && contactos[i].getNombre().equalsIgnoreCase(contactoe.getNombre())
+                        && contactos[i].getApellido().equalsIgnoreCase(contactoe.getApellido())
+                        && contactos[i].getTelefono() == contactoe.getTelefono()) {
+                    contactos[i] = null;
+                    System.out.println("\n----------------------------------------\n");
+                    System.out.println("Contacto eliminado satisfactoriamente");
+                    System.out.println("\n----------------------------------------\n");
+
+                    return;
+                }
+            }    
+        }else{
+            System.out.println("\n----------------------------------------\n");
+            System.out.println("No se encontró el contacto\nNo se pudo eliminar.");
+            System.out.println("\n----------------------------------------\n");
+        }
+        
+    }
+
+    public void modificarTelefono( String nombre, String apellido, long nuevoTelefono) {
+        Contacto contactoBuscado = obtenerContacto(nombre, apellido);
+        if(contactoBuscado!=null) {
+            for (Contacto contacto : contactos) {
+                if (contacto != null && contacto.getNombre().toLowerCase().equals(contactoBuscado.getNombre().toLowerCase()) && contacto.getApellido().toLowerCase().equals(contactoBuscado.getApellido().toLowerCase())) {
+                    contacto.setTelefono(nuevoTelefono);   
+                }
+            }
+            
+        }else {
+            System.out.println("No se encontro la persona ");
+        }
+        System.out.println("\n----------------------------------------\n");
+        System.out.println("Contacto modificado:\n");
+        System.out.println(contactoBuscado.getNombre()+" "+contactoBuscado.getApellido());
+        System.out.println("Telefono: "+nuevoTelefono);
+        System.out.println("\n----------------------------------------\n");
+    }
+    
+    public void verificarAgendaLlena(){
+        if(espaciosLibres()==0){
+            System.out.println("\n----------------------------------------\n");
+            System.out.println("¡La agenda está llena!");
+            System.out.println("\n----------------------------------------\n");
+    }else{
+        System.out.println("\n----------------------------------------\n");
+        System.out.println("Aun hay espacio disponible");
+        System.out.println("Espacio utilizado: "+(cantContactos-espaciosLibres())+" de "+cantContactos);
+        System.out.println("\n----------------------------------------\n");
+        }
     }
 
     //Metodos Auxiliares
--- src/App.java
+++ src/App.java
@@ -11,31 +11,36 @@ public class App {
         System.out.println("Para iniciar, debes escoger la capacidad de tu agenda.\nPuedes hacerlo manualmente o escoger el tamaño por defecto.\n\n¿Que deseas hacer?\n");
         System.out.println("(1) Definir manualmente.\n(2) Definir capacidad por defecto '10 contactos.'\n\nEscriba su elección:");
         int cant;
-        char opt = inputChar.next().charAt(0);
         Agenda nuevaAgenda = new Agenda();
+        boolean valid = false;
+        do{
+            char opt = inputChar.next().charAt(0);
+            switch (opt) {
+                case '1':
+                    System.out.println("Ingrese la cantidad de contactos que tu agenda permitirá: ");
+                    cant = inputNum.nextInt();
+                    nuevaAgenda = new Agenda(cant);
+                    valid = true;
+                    break;
 
-        switch (opt) {
-            case '1':
-                System.out.println("Ingrese la cantidad de contactos que tu agenda permitirá: ");
-                cant = inputNum.nextInt();
-                nuevaAgenda = new Agenda(cant);
+                case '2':
+                    nuevaAgenda = new Agenda();
+                    valid = true;
+                    break;
 
-                break;
-
-            case '2':
-                nuevaAgenda = new Agenda();
-                break;
-
-            default:
-                System.out.println("Elija una opción válida");
-                break;
+                default:
+                    System.out.println("Elija una opción válida");
+                    valid = false;
+                    break;
         }
+        }while(!valid);
+        
         
         
         char option;
         do{
             System.out.println("\n----------------------------------------\n");
-            System.out.println("::Menu de opciones::\n\n(1) Agregar nuevo Contactos.\n(2) Mostrar Lista de Contactos.\n(3) Buscar Contacto.\n(4) Eliminar Contacto.\n(5) Modificar Contacto.\n(6) Consultar espacio Disponible.\n(0) Salir"); 
+            System.out.println("::Menu de opciones::\n\n(1) Agregar nuevo Contactos.\n(2) Mostrar Lista de Contactos.\n(3) Buscar Contacto.\n(4) Eliminar Contacto.\n(5) Modificar Contacto.\n(6) Consultar espacio Disponible.\n(7) Verificar Agenda Llena.\n(0) Salir"); 
             System.out.println("\n----------------------------------------\n");
 
             System.out.println("Escoja la opción deseada: ");
@@ -46,7 +51,7 @@ public class App {
                     String nombre;
                     System.out.println("\n::::::::::Nuevo Contacto::::::::::\n");
                     do { 
-                        System.out.println("\nIngrese nombre del Contacto: ");
+                        System.out.println("Ingrese nombre del Contacto: ");
                         nombre = inputStr.nextLine();    
                         if(nombre.equals("")){
                             System.out.println("Este campo no puede estar vacío...");
@@ -80,21 +85,51 @@ public class App {
                     break;
                 
                 case '3':
+                    System.out.println("\n::::::::::Buscar Contacto::::::::::\n");
+                    System.out.println("Ingrese el nombre del contacto a buscar: ");
+                    String name = inputStr.nextLine();
+                    System.out.println("\nIngrese el apellido del contacto a buscar: ");
+                    String lastname = inputStr.nextLine();
+                    nuevaAgenda.buscarContacto(name, lastname);
                     System.out.println("Presione Enter para continuar...");
                     inputStr.nextLine();
                     break;
 
                 case '4':
+                    System.out.println("\n::::::::::Eliminar Contacto::::::::::\n");
+                    System.out.print("Ingresa el nombre del contacto a eliminar: ");
+                    String nombreEliminar = inputStr.nextLine();
+                    System.out.print("\nIngresa el apellido del contacto a eliminar: ");
+                    String apellidoEliminar = inputStr.nextLine();
+                    Contacto contactoEliminar = nuevaAgenda.obtenerContacto(nombreEliminar, apellidoEliminar);
+                    nuevaAgenda.eliminarContacto(contactoEliminar);
                     System.out.println("Presione Enter para continuar...");
                     inputStr.nextLine();
                     break;
 
                 case '5':
+                    System.out.println("\n::::::::::Modificar Telefono::::::::::\n");                    
+                    System.out.println("Ingrese nombre modificar: ");
+                    String nombreModificar = inputStr.nextLine();
+                    System.out.println("\nIngrese el apellido a modficar: ");
+                    String apellidoModificar = inputStr.nextLine();
+                    System.out.println("\nIngrese el nuevo numero: ");
+                    long telefonoModificar = inputNum.nextLong();
+                    nuevaAgenda.modificarTelefono(nombreModificar,apellidoModificar,telefonoModificar);
                     System.out.println("Presione Enter para continuar...");
                     inputStr.nextLine();
                     break;
 
                 case '6':
+                    System.out.println("\n----------------------------------------\n");
+                    System.out.println("Espacio disponible: "+nuevaAgenda.espaciosLibres()+" de "+nuevaAgenda.getCantContactos());
+                    System.out.println("\n----------------------------------------\n");
+                    System.out.println("Presione Enter para continuar...");
+                    inputStr.nextLine();
+                    break;
+                
+                case '7':
+                    nuevaAgenda.verificarAgendaLlena();
                     System.out.println("Presione Enter para continuar...");
                     inputStr.nextLine();
                     break;
